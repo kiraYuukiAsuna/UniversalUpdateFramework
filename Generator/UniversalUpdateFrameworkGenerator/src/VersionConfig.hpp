9667972637b5957cf7b2fea424a7e84eee376e4f
kiraYuukiAsuna <kirayuukiasuna@outlook.com>
2024-10-11 22:29:50 +0800

    Update elawidgettools.

--- Generator/UniversalUpdateFrameworkGenerator/src/VersionConfig.hpp
+++ Generator/UniversalUpdateFrameworkGenerator/src/VersionConfig.hpp
@@ -8,16 +8,23 @@ class AppVersionBriefInfo {
 public:
     std::string version;
     std::vector<FileManifestInfo> filemanifests;
+    std::string storage_request_url;
 
     friend void to_json(nlohmann::json&nlohmann_json_j, const AppVersionBriefInfo&nlohmann_json_t) {
         nlohmann_json_j["version"] = nlohmann_json_t.version;
         nlohmann_json_j["filemanifests"] = nlohmann_json_t.filemanifests;
+        nlohmann_json_j["storage_request_url"] =
+            nlohmann_json_t.storage_request_url;
     }
 
     friend void from_json(const nlohmann::json&nlohmann_json_j, AppVersionBriefInfo&nlohmann_json_t) {
         AppVersionBriefInfo nlohmann_json_default_obj;
         nlohmann_json_t.version = nlohmann_json_j.value("version", nlohmann_json_default_obj.version);
-        nlohmann_json_t.filemanifests = nlohmann_json_j.value("filemanifests", nlohmann_json_default_obj.filemanifests);
+        nlohmann_json_t.filemanifests = nlohmann_json_j.value(
+            "filemanifests", nlohmann_json_default_obj.filemanifests);
+        nlohmann_json_t.storage_request_url = nlohmann_json_j.value(
+            "storage_request_url",
+            nlohmann_json_default_obj.storage_request_url);
     }
 };
 
